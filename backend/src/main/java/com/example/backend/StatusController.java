cab5b1a81ae2a6cd0492e70477b0321cc83f92c4
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-20 12:00:06 +0100

    Shoppingcart

--- backend/src/main/java/com/example/backend/StatusController.java
+++ backend/src/main/java/com/example/backend/StatusController.java
@@ -1,10 +1,33 @@
 package com.example.backend;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StatusController {
+	
+	@Autowired
+	private IShoppingcartRepository repo;
+	
+	@RequestMapping(value = "shoppingcarts")
+	public List<Shoppingcart> findAllShoppingCarts() {
+		return repo.findAll();
+	}
+	
+	@RequestMapping(value = "shoppingcart")
+	public Shoppingcart shoppingcart() {
+		Shoppingcart s = new Shoppingcart();
+		s.setId(1);
+		s.setName("Apples");
+		s.setDescription("Fruits");
+		s.setAmount(50);
+		s.setDateCreatedAt(LocalDate.now());
+		return s;
+	}
 	
 	@RequestMapping(value = "status")
 	public String status() {
